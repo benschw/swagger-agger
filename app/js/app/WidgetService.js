5f956904e45a930c785edc29b8415ba3ad30dfa8
Ben Schwartz <benschw@gmail.com>
2016-03-01 23:18:47 -0600

    updates

--- app/js/app/WidgetService.js
+++ app/js/app/WidgetService.js
@@ -21,15 +21,18 @@ demo.widget.WidgetService = function($http) {
 	 */
 	this.items = [];
 
+	/**
+	 * @type {angular.$http}
+	 */
+	this.http = $http;
 
+	this.refreshAll();
+};
+
+demo.widget.prototype.refreshAll = function() {
 	var url = "http://localhost:8000/api/widget?callback=JSON_CALLBACK";
-
-	var that = this;
-
-	$http.jsonp(url)
-		.success(function(data){
-			that.items = data;
-			console.log(data);
-		});
-
+	this.http.jsonp(url).success(this.findAllSuccess);
+};
+demo.widget.prototype.findAllSuccess = function(data) {
+	this.items = data;
 };
